1e9021cf18852a48717fbad33d27123774360f31
mohamed-khaleed <mohamed.khaleed717@gmail.com>
2024-06-10 14:57:16 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="mb-1 mr-2 mt-2 bg-slate-200 text-3xl text-red-300">hi</h1>
+    </>
+  );
+}
+
+export default App;
